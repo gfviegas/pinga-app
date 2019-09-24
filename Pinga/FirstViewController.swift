beb993f792d03ba18ab694336086f9d2c40c7ac6
Gustavo Viegas <webviegas@gmail.com>
2019-09-24 20:48:19 -0300

    Initial Commit

---
+++ Pinga/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Pinga
+//
+//  Created by Gustavo Viegas on 24/09/19.
+//  Copyright © 2019 Gustavo Viegas. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pinga/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Pinga
+//
+//  Created by Gustavo Viegas on 24/09/19.
+//  Copyright © 2019 Gustavo Viegas. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
